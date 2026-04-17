c7a5f06650632682aa91a08f18f831b6013ebaa5
syedzeeshanusrathi-DevOps <syedzeeshanusrathi@gmail.com>
2026-04-17 16:31:20 -0400

    Add firebase config for deployment

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDmRCF2Y3Hd11Ne-uC0-HTu8zFe8pFjy3w",
+  authDomain: "my-library-d1721.firebaseapp.com",
+  projectId: "my-library-d1721",
+  storageBucket: "my-library-d1721.firebasestorage.app",
+  messagingSenderId: "769789629851",
+  appId: "1:769789629851:web:0dabc8b8a26a9a0ef23d3e",
+  measurementId: "G-M5M03S7Y33"
+};
